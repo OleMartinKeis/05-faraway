464cd7d172b2520866cc56635e1f17dd1045219b
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-07 12:36:18 +0100

    Added description and number of items

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Form() {
+function Form({ onAddItems }) {
+    const [description, setDescription] = useState("");
+    const [numb, setNumb] = useState(1);
+
     function handleSubmit(e) {
         e.preventDefault();
+
+        if (!description) return;
+        const newItem = {
+            description,
+            quantity: numb,
+            packed: false,
+            id: Date.now(),
+        };
+
+        onAddItems(newItem);
+
+        setDescription("");
+        setNumb(1);
     }
 
     return (
         <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-            <select>
+            <select
+                value={numb}
+                onChange={(e) => setNumb(Number(e.target.value))}
+            >
                 {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                     <option value={num} key={num}>
                         {num}
                     </option>
                 ))}
             </select>
-            <input type="text" placeholder="Item..." />
+            <input
+                type="text"
+                placeholder="Item..."
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
             <button>Add</button>
         </form>
     );
