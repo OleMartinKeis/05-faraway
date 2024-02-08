1fb5e39c4bc1441bf98ced046b800cc0c361c78f
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-08 09:04:56 +0100

    Added calculations of how many items are packed

--- src/components/stats/index.jsx
+++ src/components/stats/index.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-function Stats() {
+function Stats({ items }) {
+    if (!items.length)
+        return (
+            <p className="stats">
+                <em>Start adding some items to you list 🚀</em>
+            </p>
+        );
+
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
     return (
         <footer className="stats">
-            <em>🧳 You have X items on your list, and you already packed X </em>
+            <em>
+                {percentage == 100
+                    ? "You got everything! Ready to go 🛫"
+                    : `🧳 You have ${numItems} items on your list, and you alreadypacked ${numPacked} (${percentage}%)`}
+            </em>
         </footer>
     );
 }
