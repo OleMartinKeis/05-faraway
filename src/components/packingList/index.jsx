c20e7bf26706ef7725e8c454e7b10902101d14da
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-02-08 09:04:38 +0100

    Removed initial items and added delete and toggle

--- src/components/packingList/index.jsx
+++ src/components/packingList/index.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 
-const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-    { id: 3, description: "Toothbrush", quantity: 1, packed: false },
-    { id: 4, description: "Toothpaste", quantity: 1, packed: false },
-    { id: 5, description: "Sandals", quantity: 2, packed: false },
-    { id: 6, description: "T-shirts", quantity: 6, packed: false },
-    { id: 7, description: "Shorts", quantity: 5, packed: false },
-];
-
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
     return (
         <div className="list">
             <ul>
                 {items.map((item) => (
-                    <Item item={item} key={item.id} />
+                    <Item
+                        item={item}
+                        key={item.id}
+                        onDeleteItem={onDeleteItem}
+                        onToggleItem={onToggleItem}
+                    />
                 ))}
             </ul>
         </div>
     );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
     return (
         <li>
+            <input
+                type="checkbox"
+                value={item.packed}
+                onChange={() => onToggleItem(item.id)}
+            />
             <span style={item.packed ? { textDecoration: "line-through" } : {}}>
                 {item.quantity} {item.description}
             </span>
-            <button>❌</button>
+            <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
     );
 }
